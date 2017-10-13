14d6becfce74f018be7ef64d7d10033674f92e56
Chris Hamons <chris.hamons@xamarin.com>
2017-10-13 12:49:02 -0500

    Add test project for lib/ref issue. Does not repro yet

---
+++ XI_nuget_libref/Test/NugetTest.Mac/NugetTest.Basic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NugetTest.Basic
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(TestLib.MyClass.Type);
+		}
+	}
+}
---
+++ XI_nuget_libref/Test/NugetTest.Mac/NugetTest.MacFull/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace NugetTest.MacFull
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
