7d5eb8a0f4f0407bed3fd5c6f5f7071d48ed6bad
Chris Hamons <chris.hamons@xamarin.com>
2018-07-27 11:50:33 -0500

    Add path example

---
+++ dotnet-tool-path/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_tool_path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
