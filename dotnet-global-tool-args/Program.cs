0cca1713421662f4ed62938b93361906b214fcfd
Chris Hamons <chris.hamons@xamarin.com>
2018-07-27 12:09:00 -0500

    Add example of not being able to run tool in project dir

---
+++ dotnet-global-tool-args/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_global_tool_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ($"Hello: {string.Join (" ", args)}");
+        }
+    }
+}
