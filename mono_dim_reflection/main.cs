ca162cab0020ab4b8f586bbe4c4853298bbc5068
Chris Hamons <chris.hamons@xamarin.com>
2018-10-16 09:09:13 -0500

    Add correct, but confusing example

--- mono_dim_reflection/main.cs
+++ mono_dim_reflection/main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Library;
 
 namespace Main
@@ -15,9 +17,16 @@ namespace Main
 	{
 		public static void Main ()
 		{
-			CoffeeBar bar = new CoffeeBar ();
-			bar.Charge (2);
-			bar.Brew (1);
+			ICoffeeBar barAsInterface = new CoffeeBar ();
+			ICoffeeBar barAsConcrete = new CoffeeBar ();
+
+			Console.WriteLine ("Methods on CoffeeBar:");
+			Console.WriteLine (string.Join ("\n", typeof (CoffeeBar).GetMethods ().Select (x => "\t" + x.Name)));
+			Console.WriteLine ("Methods on ICoffeeBar:");
+			Console.WriteLine (string.Join ("\n", typeof (ICoffeeBar).GetMethods ().Select (x => "\t" + x.Name)));
+
+			typeof (ICoffeeBar).GetMethod ("Brew").Invoke (barAsInterface, new object [] { 1 });
+			typeof (ICoffeeBar).GetMethod ("Brew").Invoke (barAsConcrete, new object [] { 1 });
 		}
 	}
 }
